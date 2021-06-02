e57f971727ca72a09411f3f4b6272545cc08f62e
Paradox <atsedebenham@gmail.com>
2021-06-02 18:43:08 +0100

    Update crafting.py

--- cogs/crafting.py
+++ cogs/crafting.py
@@ -62,8 +62,12 @@ class Crafting(commands.Cog):
         page = 0
         userstripes = await self.executesql("SELECT inv.unique_item_id, inv.image_id, inv.count FROM inventories inv INNER JOIN images i USING(image_id) WHERE inv.user_id = ? AND inv.server_id = ? AND inv.count > 1 AND (i.text LIKE '%stripe')", (ctx.author.id, ctx.guild.id))
         remainingstripes = await self.executesql("SELECT SUM(inv.count) FROM inventories inv INNER JOIN images i USING(image_id) WHERE inv.user_id = ? AND inv.server_id = ? AND inv.count < 2 AND (i.text LIKE '%stripe')", (ctx.author.id, ctx.guild.id))
+        if remainingstripes is None:
+            remainingstripes = 0
+        else:
+            remainingstripes = remainingstripes[0][0]
         if len(userstripes) < 11:
-            return await ctx.send(f"You don't have enough stripes to craft a flag, please try again when you have a flag's worth\nRemaining colours: {11-len(remainingstripes[0][0])}")
+            return await ctx.send(f"You don't have enough stripes to craft a flag, please try again when you have a flag's worth\nRemaining stripes: {22 - remainingstripes]}")
 
         embed = discord.Embed(title='Crafting Menu',
                               description='Loading...',
